ac216ea14c861d87ffcf8d89860a26bc6d1cbc09
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-06-21 15:44:31 +0900

    ロゴとスピナーをきれいに中央に配置

--- loader.js
+++ loader.js
@@ -2,8 +2,12 @@ function loadStart(){
     var height = $(window).height(); 
     $(".wrapper").height(height).show();
     $(".loading-sign").show();
+    $("#kakuninkun-logo").show();
+    $("#spinner").show();
 }
 function loadEnd(){
     $(".wrapper").fadeOut(1000);
-    $(".loading-sign").fadeOut(700);  
+    $(".loading-sign").fadeOut(700);
+    $("#kakuninkun-logo").fadeOut(700); 
+    $("#spinner").fadeOut(700); 
 }
