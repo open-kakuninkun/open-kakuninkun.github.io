c4dd692579149fd1cf2e93979f34390f88a67a0f
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-07-11 14:47:58 +0900

    Service Worker 有効化(試験的)

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', function (event) {
+
+});
+
+self.addEventListener('activate', function (event) {
+
+});
+
+self.addEventListener('fetch', function (event) {
+
+});
