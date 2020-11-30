e21488e5e907212e21d50f5ed94dd23bb780de29
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-11-30 14:47:46 +0900

    Cookie consentを有効化

---
+++ cookieconsent_init.js
@@ -0,0 +1,17 @@
+window.cookieconsent.initialise({
+   "palette": {
+    "popup": {
+      "background": "#eb6c44",
+      "text": "#ffffff"
+     },
+    "button": {
+    "background": "#f5d948"
+    }
+},
+"theme": "classic",
+"content": {
+ "message": "このサイトでは、Cookieが有効かどうかを調べるために、Cookieを使用します。",
+ "dismiss": "OK",
+ "link": "ポリシー"
+}
+});
